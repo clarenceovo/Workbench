9543cc3fd5ae0a97efe0ef7fdc609741f671ee3d
clarencemini <clarenceovo@gmail.com>
2025-06-02 22:51:57 +0800

    update pm2

---
+++ Workbench/Cronjob/ecosystem.config.js
@@ -0,0 +1,24 @@
+const code_path = '/home/coin_nlsresearch/Project/Workbench';
+
+module.exports = {
+  apps: [{
+    name: 'HLSnapshotter',
+    script: './Workbench/Cronjob/HLSnapshotter.py',
+    cwd: code_path,
+    env: {
+      PYTHONUNBUFFERED: "1",
+      PYTHONPATH: code_path,
+    },
+    interpreter: '/home/coin_nlsresearch/miniconda3/envs/CryptoBot/bin/python',
+    exec_mode: 'fork',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    error_file: `${code_path}/Workbench/Cronjob/logs/error.log`,
+    out_file: `${code_path}/Workbench/Cronjob/logs/out.log`,
+    merge_logs: true,
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+    cron_restart: '0 0 * * *'
+  }]
+};
