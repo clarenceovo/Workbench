b9b94ab4ad4b3d5c15a15616546722ed472969d6
clarencemini <clarenceovo@gmail.com>
2025-06-07 11:14:59 +0800

    update bots pm2 config

--- Workbench/StrategyBot/ecosystem.config.js
+++ Workbench/StrategyBot/ecosystem.config.js
@@ -1,13 +1,26 @@
+const code_path = '/home/coin_nlsresearch/Project/Workbench';
 
 module.exports = {
   apps: [
     {
-      name: "SwapArbStrategyBot",
-      script: "./Workbench/StrategyBot/SwapArbStrategyBot.py",
-      interpreter: "python3", // Specify Python interpreter
-      instances: 1, // Number of instances
-      autorestart: true, // Automatically restart on failure
-      watch: false
+      name: 'SwapArbStrategyBot',
+      script: './Workbench/StrategyBot/SwapArbStrategyBot.py',
+      cwd: code_path,
+      env: {
+        PYTHONUNBUFFERED: "1",
+        PYTHONPATH: code_path,
+      },
+      interpreter: '/home/coin_nlsresearch/miniconda3/envs/CryptoBot/bin/python',
+      exec_mode: 'fork',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      error_file: `${code_path}/Workbench/StrategyBot/logs/error.log`,
+      out_file: `${code_path}/Workbench/StrategyBot/logs/out.log`,
+      merge_logs: true,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      cron_restart: '0 0 * * *'
     },
   ],
 };
